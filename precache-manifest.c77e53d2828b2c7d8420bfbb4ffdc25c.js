2db0f33caf844eeb28397baa5f8c881ce68bdd42
Andreas Pashalides <pashalides@apiomat.com>
2020-04-16 22:48:00 +0200

    Updates

---
+++ precache-manifest.c77e53d2828b2c7d8420bfbb4ffdc25c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc1fc75d502548082ff1763b49e0a33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2017b410447d15aafbd6",
+    "url": "/static/css/main.9cbc0e2e.chunk.css"
+  },
+  {
+    "revision": "f91091d61f91e61e3229",
+    "url": "/static/js/2.1f1f7725.chunk.js"
+  },
+  {
+    "revision": "fc943b98bb19e204f59004c09341b414",
+    "url": "/static/js/2.1f1f7725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2017b410447d15aafbd6",
+    "url": "/static/js/main.ae217e4e.chunk.js"
+  },
+  {
+    "revision": "55eb75a8973b343e3ff8",
+    "url": "/static/js/runtime-main.1b0872d1.js"
+  }
+]);
