113fddf20c925db21b74e1c744ac459541513b64
Andreas Pashalides <pashalides@apiomat.com>
2020-04-18 12:19:32 +0200

    Updates

--- precache-manifest.15ec8f7ce060076e76e02a195dd813db.js
+++ precache-manifest.15ec8f7ce060076e76e02a195dd813db.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df7f0e0f09cc7386c76484c30f9237fb",
+    "revision": "f70dbd8788c81a4d463745cad887beb2",
     "url": "/ZusammenAlleinChallenges/index.html"
   },
   {
-    "revision": "9fba8c075ba2dddb7d0d",
+    "revision": "12b952086bb96ea7b625",
     "url": "/ZusammenAlleinChallenges/static/css/main.29aed0d8.chunk.css"
   },
   {
-    "revision": "ee898608290b100b7ac6",
-    "url": "/ZusammenAlleinChallenges/static/js/2.215ff2f5.chunk.js"
+    "revision": "c486c1a283385a48ec7c",
+    "url": "/ZusammenAlleinChallenges/static/js/2.682ccf4d.chunk.js"
   },
   {
     "revision": "2de0619936145078fdaa9873b2c206cd",
-    "url": "/ZusammenAlleinChallenges/static/js/2.215ff2f5.chunk.js.LICENSE.txt"
+    "url": "/ZusammenAlleinChallenges/static/js/2.682ccf4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9fba8c075ba2dddb7d0d",
-    "url": "/ZusammenAlleinChallenges/static/js/main.8cec8b92.chunk.js"
+    "revision": "12b952086bb96ea7b625",
+    "url": "/ZusammenAlleinChallenges/static/js/main.92a5dc64.chunk.js"
   },
   {
     "revision": "cc7fc452daadea6153ec",
