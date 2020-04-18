ae900393d52187a3de83881d79837c4fcfa112cc
Andreas Pashalides <pashalides@apiomat.com>
2020-04-18 14:45:11 +0200

    fix word break in image

--- src/services/play-card-picture-service.ts
+++ src/services/play-card-picture-service.ts
@@ -6,7 +6,7 @@ const createPicture = async (): Promise<Blob> => {
   const documentRect = document.querySelector('body')?.getBoundingClientRect()
   const rect = playcard.getBoundingClientRect()
 
-  const canvas = await html2canvas(playcard, { y: Math.abs(documentRect!.top - rect.top), scale: 1 })
+  const canvas = await html2canvas(playcard, { y: Math.abs(documentRect!.top - rect.top), scale: 1})
 
   return new Promise((resolve, reject) => {
     canvas.toBlob((blob) => {
