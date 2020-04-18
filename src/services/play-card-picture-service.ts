50d742867e72e9431f7c04e10cd55a7a139f38a4
Andreas Pashalides <pashalides@apiomat.com>
2020-04-18 11:38:13 +0200

    fixed positioning in generated picture

--- src/services/play-card-picture-service.ts
+++ src/services/play-card-picture-service.ts
@@ -3,9 +3,10 @@ import { saveAs } from 'file-saver'
 
 const createPicture = async (): Promise<Blob> => {
   const playcard = document.querySelector('#play-card') as HTMLElement
+  const documentRect = document.querySelector('body')?.getBoundingClientRect()
   const rect = playcard.getBoundingClientRect()
 
-  const canvas = await html2canvas(playcard, { scrollY: rect.y })
+  const canvas = await html2canvas(playcard, { y: Math.abs(documentRect!.top - rect.top), scale: 1 })
 
   return new Promise((resolve, reject) => {
     canvas.toBlob((blob) => {
